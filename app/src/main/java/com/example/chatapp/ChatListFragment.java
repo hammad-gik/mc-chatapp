85be2f9458db0c9adc5295c877b39967de9ff94b
Hammad Mehmood <hammadmehmood@gmail.com>
2021-04-28 11:26:09 +0500

    initial commit

---
+++ app/src/main/java/com/example/chatapp/ChatListFragment.java
@@ -0,0 +1,120 @@
+package com.example.chatapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.example.chatapp.models.ChatDialog;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
+import com.stfalcon.chatkit.commons.ImageLoader;
+import com.stfalcon.chatkit.dialogs.DialogsList;
+import com.stfalcon.chatkit.dialogs.DialogsListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ * create an instance of this fragment.
+ */
+public class ChatListFragment extends Fragment {
+
+    ArrayList<ChatDialog> chats = new ArrayList<>();
+
+    FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+    DialogsList dialogsList;
+
+    Button startChatButton;
+
+    public ChatListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_chat_list, container, false);
+
+        dialogsList = (DialogsList) view.findViewById(R.id.chats_list);
+
+        startChatButton = (Button) view.findViewById(R.id.start_chat_button);
+
+        DialogsListAdapter dialogsListAdapter = new DialogsListAdapter<ChatDialog>(new ImageLoader() {
+            @Override
+            public void loadImage(ImageView imageView, @Nullable String url, @Nullable Object payload) {
+
+                if(!url.equals(""))
+                    Picasso.get().load(url).into(imageView);
+            }
+        });
+
+        dialogsList.setAdapter(dialogsListAdapter);
+
+        startChatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startChat("My first chat");
+            }
+        });
+
+        /*
+        ChatDialog chatDialog = new ChatDialog();
+        chatDialog.id = "123";
+        chatDialog.dialogPhoto = "";
+        chatDialog.dialogName = "first chat";
+        chatDialog.unreadCount = 0;
+
+        dialogsListAdapter.addItem(chatDialog);*/
+
+        return view;
+    }
+
+    private void startChat(String firstMessage) {
+
+        // Create a new user with a first and last name
+        Map<String, Object> chat = new HashMap<>();
+        chat.put("id", "123");
+        chat.put("dialogPhoto", "");
+        chat.put("dialogName", firstMessage);
+        chat.put("unreadCount", 0);
+
+        firestore.collection("chats")
+                .add(chat)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("Chat List", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("Chat List", "Error adding document", e);
+                    }
+                });
+    }
+}
